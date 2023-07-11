ff0566abd11f4396e8f08a446d072ea4bc8b319d
Ivan-Tulcan <ivan.tulcan@udla.edu.ec>
2023-07-10 20:52:19 -0500

    Add project files.

---
+++ SysAid/Model/data.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SysAid.Model
+{
+    public class data
+    {
+        private float efectividad;
+        private float residual;
+
+        public data(float efectividad, float residual)
+        {
+            this.efectividad = efectividad;
+            this.residual = residual;
+        }
+
+        public float Efectividad { get => efectividad; set => efectividad = value; }
+        public float Residual { get => residual; set => residual = value; }
+
+        public float calcularResidual(float riesgo)
+        {
+            float resultado;
+            resultado = this.efectividad * riesgo;
+            return resultado;
+        }
+    }
+}
